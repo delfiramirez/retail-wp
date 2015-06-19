3eafb203cf9820e17a156f70dcc0dd139a9c0b0b
Segonquart Studio {public repositories} <delfiramirez@users.noreply.github.com>
2015-06-19 02:21:47 +0200

    Update index.php

--- rotulossagarra/themes/sagarra/index.php
+++ rotulossagarra/themes/sagarra/index.php
@@ -12,14 +12,17 @@ get_header();
 ?>
 
 <div id="home-sagarra">
+    
     <?php $attachments = new Attachments('attachments'); ?>
 
     <div class="sagarra">
 
         <?php if ($attachments->exist()) : ?>
+        
             <div class="gallery">
-
+                
                 <?php while ($attachments->get()) : ?>
+                
                     <dl>
                         <dt>
                         <?php echo $attachments->image('article-thumb'); ?>
@@ -36,23 +39,28 @@ get_header();
                     $args = array('numberposts' => 1,
                         'category__not_in' => array(8, 6)
                     );
-
                     $myposts = get_posts($args);
                     foreach ($myposts as $post) : setup_postdata($post);
                         ?>
                         <dt class="bannerUltimos">
+                            
                         <?php if (has_post_thumbnail()) : ?>
+                        
                             <?php if (qtrans_getLanguage() == 'es'): ?>
                                 <a href="<?php echo get_permalink(1928); ?>">
                                     <img class="nuevosProductos" src="http://rotulossagarra.com/static/images/last-projects/cast.png" alt="<?php ?>" />
                                 <?php endif; ?>
+                                
                                 <?php if (qtrans_getLanguage() == 'en'): ?>
                                     <a href="<?php echo get_permalink(1928); ?>">
                                         <img class="nuevosProductos" src="http://rotulossagarra.com/static/images/last-projects/eng.png" alt="<?php ?>" />
                                     <?php endif; ?>
+                                    
                                     <?php if (qtrans_getLanguage() == 'ca'): ?>
+                                    
                                         <a href="<?php echo get_permalink(1928); ?>">
                                             <img class="nuevosProductos" src="http://rotulossagarra.com/static/images/last-projects/cat.png" alt="<?php ?>" />
+                                       
                                         <?php endif; ?>
 
                                         <?php echo get_the_post_thumbnail($post_id, 'article-thumb'); ?>
@@ -63,9 +71,9 @@ get_header();
                             endforeach;
                             wp_reset_postdata();
                             ?>
-                            </dl>
-                            </div>
-                        <?php endif; ?>
-                        </div>
-                        </div>
-                        <?php get_footer(); ?>
+              </dl>
+        </div>
+     <?php endif; ?>
+     </div>
+</div>
+ <?php get_footer(); ?>
